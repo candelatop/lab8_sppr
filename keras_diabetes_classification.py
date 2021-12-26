7e528757cea421b58d8d412e9c16783c8170c1a8
candelatop <79348834+candelatop@users.noreply.github.com>
2021-12-26 19:07:24 +0300

    Update keras_diabetes_classification.py

--- keras_diabetes_classification.py
+++ keras_diabetes_classification.py
@@ -3,32 +3,32 @@ from keras.layers import Dense, Dropout
 from sklearn.model_selection import train_test_split
 import numpy
 
-# random seed for reproducibility
+# задаем для воспроизводимости результатов
 numpy.random.seed(2)
 
-# loading load prima indians diabetes dataset, past 5 years of medical history 
+# загружаем датасет, соответствующий последним пяти годам до определение диагноза 
 dataset = numpy.loadtxt("prima-indians-diabetes.csv", delimiter=",")
 
-# split into input (X) and output (Y) variables, splitting csv data
+# разбиваем датасет на матрицу параметров (X) и вектор целевой переменной (Y)
 X = dataset[:,0:8]
 Y = dataset[:,8]
-
-# split X, Y into a train and test set
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-# create model, add dense layers one by one specifying activation function
+# создаем модели, добавляем слои один за другим
 model = Sequential()
-model.add(Dense(15, input_dim=8, activation='relu')) # input layer requires input_dim param
+model.add(Dense(15, input_dim=8, activation='relu')) # входной слой требует задать input_dim
 model.add(Dense(10, activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dropout(.2))
-model.add(Dense(1, activation='sigmoid')) # sigmoid instead of relu for final probability between 0 and 1
+model.add(Dense(1, activation='sigmoid')) # сигмоида вместо relu для определения вероятности
 
-# compile the model, adam gradient descent (optimized)
+# компилируем модель
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
-# call the function to fit to the data (training the network)
+# обучаем нейронную сеть
 model.fit(x_train, y_train, epochs = 1000, batch_size=20, validation_data=(x_test, y_test))
 
-# save the model
+# сохраняем и выводим
 model.save('weights.h5')
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
